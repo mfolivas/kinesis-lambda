eeab945ab751cf6bbe9d8a6100a0d71625b0bb39
felipe.gonzalez <felipe.gonzalez@spafinder.com>
2016-04-26 13:35:36 -0400

    Making non-static

--- src/main/java/com/mfolivas/examples/aws/lambda/ProcessKinesisEvents.java
+++ src/main/java/com/mfolivas/examples/aws/lambda/ProcessKinesisEvents.java
@@ -3,24 +3,28 @@ package com.mfolivas.examples.aws.lambda;
 import com.amazonaws.services.lambda.runtime.events.KinesisEvent;
 
 import java.io.IOException;
+import java.util.Optional;
 
 /**
  * @author Marcelo Olivas
  */
 public class ProcessKinesisEvents {
     public static void recordHandler(KinesisEvent event) throws IOException {
-        
-        event.getRecords()
-        .stream()
-        .forEach(rec -> {
-            System.out.format("Event [%s-%s](vesion %s) from :%s: stored-data >%s<\n"
-                    , rec.getEventID()
-                    , rec.getEventName()
-                    , rec.getEventVersion()
-                    , rec.getEventSource()
-                    , new String(rec.getKinesis().getData().array())
-                    );
-            });
+        Optional.ofNullable( event.getRecords())
+        .map(records -> records.stream())
+        .ifPresent(stream -> {
+            stream.forEach(rec -> {
+                System.out.format("Event [%s]-[%s](vesion %s) from :%s: stored-data >%s<\n"
+                        , rec.getEventID()
+                        , rec.getEventName()
+                        , rec.getEventVersion()
+                        , rec.getEventSource()
+                        , rec.getInvokeIdentityArn()
+                        , new String(rec.getKinesis().getData().array())
+                        );
+                
+                });
+        });
     }
 }
 
